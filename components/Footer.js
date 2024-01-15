ec13f7349c36f08121eae6b67f47ca9c41d42995
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-01-15 16:15:44 +0530

    dbjgjd

--- components/Footer.js
+++ components/Footer.js
@@ -7,44 +7,59 @@ import {
 
 
 const FooterLinks = [
-  {
-    title: "Services",
-    link: "services",
-  },
-  // {
-  //   title: "Insights",
-  //   link: "/insights",
-  // },
-  {
-    title: "About",
-    link: "about",
-  },
+  
+    {
+        id: 1,
+         
+        title: "Lybley IT Services",
+        link: "https://it.lybley.com",
+    },
+    {
+        id: 2,
+       
+        title: "Lybley Home Appliances",
+        link: "https://lybley.com",
 
-  {
-    title: "Blogs",
-    link: "blogs",
-  },
-  {
-    title: "Careers",
-    link: "careers",
-  },
-  {
-    title: "Contact us",
-    link: "contact",
-  },
+
+    },
+    {
+        id: 3,
+        
+        title: "Samosafry",
+        link: "https://www.samosafry.com",
+ 
+    },
+    {
+        id: 4,
+      
+        title: "Sparetrade",
+        link: "https://www.sparetrade.in",
+    },
+    {
+        id: 4,
+        
+        title: "Heuser",
+        link: "https://heuser.in",
+    },
+     
 ];
+  
+ 
 
 const Footer = () => {
+  const handleLink=(link )=>{
+    window.open(link,"_blank");
+}
   return (
     <>
       <div className="dark:bg-gray-950  relative overflow-hidden  ">
         <div className="bg-[#000] text-white  ">
           <div className="container mx-auto px-14">
             {/* <div className="grid md:grid-cols-3 py-5 bg-white/80 backdrop-blur-sm rounded-t-xl"> */}
-            <div className="grid md:grid-cols-3 py-5   rounded-t-xl">
+            <div className="grid md:grid-cols-3  pt-4   rounded-t-xl">
               <div className="py-8 ">
                 <h1 className="flex items-center gap-3 text-xl sm:text-3xl font-bold text-justify sm:text-left">
-                  <Link href={"/"}> <img src={"/Logo.png"} alt="" className="max-h-[64px] rounded-lg" /></Link>
+                  <Link href={"/"}> <img src={"/Logo.png"} alt="" className="max-h-[64px] rounded-lg border-2 border-white" /></Link>
                   {/* TravelloGo */}
                 </h1>
                 <p className="text-sm mt-4 ">
@@ -68,27 +83,26 @@ const Footer = () => {
               </div>
               <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
                 <div>
-                  <div className="py-8 px-4">
+                  <div className="pt-8 px-4">
                     <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
-                      Important Links
+                    Our Brands
                     </h1>
                     <ul className="flex flex-col gap-3">
                       {FooterLinks.map((link,index) => (
-                        <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-700 dark:text-gray-200" key={index}>
-                          <Link
-                            href={link.link}
-                            onClick={() => window.scrollTo(0, 0)}
+                        <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1    hover:text-blue-400" key={index}>
+                          <div
+                            onClick={()=>handleLink(link?.link)}
                           >
                             <span>&#11162;</span>
-                            <span>{link.title}</span>
-                          </Link>
+                            <span>{link?.title}</span>
+                          </div>
                         </li>
                       ))}
                     </ul>
                   </div>
                 </div>
                 <div className="block md:hidden">
-                <div className="py-8 px-4">
+                <div className="pt-8 px-4">
                     <h1 className="text-xl font-bold text-justify sm:text-left mb-3">
                      Social Media
                     </h1>
@@ -101,7 +115,7 @@ const Footer = () => {
                 </div>
               </div>
                 <div className="col-span-2">
-                  <div className="py-8 px-4">
+                  <div className="pt-8 px-4">
                     <div className="rounded-xl  ">
                       <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.783672779409!2d77.36534197457178!3d28.606265885274766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cef5e6156c623%3A0xfc1292095a4933f9!2sLYBLEY!5e0!3m2!1sen!2sin!4v1704344578953!5m2!1sen!2sin"
